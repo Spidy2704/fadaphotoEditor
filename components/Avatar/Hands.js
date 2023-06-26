86da2ffd82b0acce96046ba4e2ba5e6759c351d7
sulaiman282 <spidy.twitch.tv@gmail.com>
2023-06-27 02:35:15 +0600

    taco tuesday

--- components/Avatar/Hands.js
+++ components/Avatar/Hands.js
@@ -5,9 +5,14 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import handjson from "../../public/hands/Hands.json";
-import ImageSmall from "./imageSmall"
+import ImageSmall from "./imageSmall";
 
-export default function Hands({ avatarValue, name,setActiveOverlay,activeOverlay }) {
+export default function Hands({
+  avatarValue,
+  name,
+  setActiveOverlay,
+  activeOverlay,
+}) {
   const [expanded, setExpanded] = useState(null);
   console.log("main json", handjson);
   console.log("name", name);
@@ -42,21 +47,53 @@ export default function Hands({ avatarValue, name,setActiveOverlay,activeOverlay
               className="border  overflow-clip border-primary"
             >
               <AccordionSummary
-                className={`${Object.keys(item)[0] == 'New Arrivals!' ? 'bg-[#F3FCFF]' : 'bg-primary'} `}
+                className={`${
+                  Object.keys(item)[0] === "New Arrivals!"
+                    ? "bg-[#F3FCFF]"
+                    : Object.keys(item)[0] === "Taco Tuesday"
+                    ? "bg-green-700"
+                    : "bg-primary"
+                } `}
                 // New Arrivals!
-                expandIcon={<ExpandMoreIcon className={`${Object.keys(item)[0] == 'New Arrivals!' ? 'text-[#9F1B4C]' : 'text-white'} `} />}
+                expandIcon={
+                  <ExpandMoreIcon
+                    className={`${
+                      Object.keys(item)[0] == "New Arrivals!"
+                        ? "text-[#9F1B4C]"
+                        : "text-white"
+                    } `}
+                  />
+                }
                 aria-controls={`panel${index}-content`}
                 id={`panel${index}-header`}
               >
-                <p className={`lg:text-lg text-base font-bold  ${Object.keys(item)[0] == 'New Arrivals!' ? 'text-[#9F1B4C]' : 'text-white'} `}>{`${Object.keys(item)[0]}`}</p>
+                <p
+                  className={`lg:text-lg text-base font-bold ${
+                    Object.keys(item)[0] === "New Arrivals!"
+                      ? "text-[#9F1B4C]"
+                      : "text-white"
+                  }`}
+                >
+                  {`${Object.keys(item)[0]}`}{" "}
+                  {Object.keys(item)[0] === "Taco Tuesday" && (
+                    <span>&#127790; (Available Only on Tuesdays)</span> 
+                  )}
+                </p>
               </AccordionSummary>
-              <AccordionDetails >
-                <ImageSmall setActiveOverlay={setActiveOverlay} activeOverlay={activeOverlay} item={item} avatarValue={avatarValue} />
+              <AccordionDetails>
+                <ImageSmall
+                  setActiveOverlay={setActiveOverlay}
+                  activeOverlay={activeOverlay}
+                  item={item}
+                  avatarValue={avatarValue}
+                />
               </AccordionDetails>
             </Accordion>
           ))
         ) : (
-          <p className="lg:text-4xl md:text-3xl text-2xl h-40">Categories not avaible now. Please comeback later.</p>
+          <p className="lg:text-4xl md:text-3xl text-2xl h-40">
+            Categories not avaible now. Please comeback later.
+          </p>
         )}
       </div>
     </div>
